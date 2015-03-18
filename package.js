2f733100a7ad0182fe60f66f5ea54faf6e06f35a
jmanero <john.manero@gmail.com>
2015-03-17 21:23:09 -0400

    Update package.js to version 1.0.0 interface and remove debug logging from controller

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
-var Package = require('auto-package');
+Package = require('auto-package').scheme('1.0');
 
-Package.name('menagerie');
-Package.version_file();
-Package.author('John Manero', 'john.manero@gmail.com');
-Package.description('A distributed, highly available versioned resource directory service');
+Package.name = 'menagerie';
+Package.versionFile();
+Package.author = 'John Manero <john.manero@gmail.com>';
+Package.description = 'A distributed, highly available versioned resource directory service';
 
 Package.depends('body-parser', '1.x');
 Package.depends('express', '4.x');
@@ -12,11 +12,11 @@ Package.depends('nconf', '0.7.x');
 Package.depends('node-etcd', '3.0.x');
 Package.depends('semver', '4.x');
 
-Package.github_repo('jmanero/menagerie');
+Package.githubRepo('jmanero/menagerie');
 
 Package.keyword('distributed');
 Package.keyword('versioned');
 Package.keyword('service');
 Package.keyword('directory');
 
-Package.license('MIT');
+Package.license = 'MIT';
